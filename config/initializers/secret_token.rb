fa76ff2b6bb59c79155174a7f3b69ab584d68636
Elizabeth Rapoport <elizrap@mit.edu>
2014-01-18 14:35:24 -0500

    initial commit: change gemfile and .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '20138007b84e5a99df1fdc413b4eaa8d24f7623f22fa9bc224c713c9aea1ff00d6b07b74b33d7a03b6402a884fc634b929584d24ef066a355be9865bc05615a3'
